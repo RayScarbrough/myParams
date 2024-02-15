b2c13a9b19c08e4b835a129189216278637b610f
raysc <raysc@RYA>
2024-02-14 21:53:29 -0500

    Add project files.

---
+++ myParams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random r = new Random();
+        Console.Write("How many random numbers do you want to generate? ");
+        int count = int.Parse(Console.ReadLine());
+        int[] numbers = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            numbers[i] = r.Next(1, 100); 
+        }
+
+        Console.WriteLine("Generated numbers:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Total of the numbers array = {Add(numbers)}");
+        Console.WriteLine($"Product of the numbers array = {Multiply(numbers)}");
+    }
+
+    static int Add(params int[] numbers)
+    {
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        return total;
+    }
+
+    static int Multiply(params int[] numbers)
+    {
+        int total = 1; 
+        foreach (int number in numbers)
+        {
+            total *= number;
+        }
+        return total;
+    }
+}
